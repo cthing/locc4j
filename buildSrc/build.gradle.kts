ea9e7fed62dc19e3878bdd7f2be2d3d3c6aa56a4
Baron Roberts <baron@cthing.com>
2024-02-14 09:54:35 -0800

    Do not upgrade Checkstyle until 14123 is properly fixed.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -31,12 +31,6 @@ dependencies {
     implementation(libs.jacksonDatabind)
 
     spotbugsPlugins(libs.spotbugsContrib)
-
-    modules {
-        module("com.google.collections:google-collections") {
-            replacedBy("com.google.guava:guava", "google-collections is now part of Guava")
-        }
-    }
 }
 
 val checkstyleDevDir: File = file("../dev/checkstyle")
