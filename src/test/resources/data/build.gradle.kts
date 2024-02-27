3ecc581903fdb2a56f22526c5b78ddfe43d1a35d
Baron Roberts <baron@cthing.com>
2024-02-26 16:00:20 -0800

    Add Gradle and Maven build files to supported languages.

---
+++ src/test/resources/data/build.gradle.kts
@@ -0,0 +1,234 @@
+package data
+
+// Comment
+
+import com.github.spotbugs.snom.Effort
+import com.github.spotbugs.snom.Confidence
+import org.cthing.locc4j.plugins.LanguagePlugin
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+/*
+ * Comment
+ */
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    `java-library`
+    checkstyle
+    jacoco
+    `maven-publish`
+    signing
+    alias(libs.plugins.spotbugs)
+    alias(libs.plugins.versions)
+    id("org.cthing.locc4j.language")
+}
+
+val baseVersion = "1.0.0"
+val isSnapshot = true
+
+val isCIServer = System.getenv("CTHING_CI") != null
+val buildNumber = if (isCIServer) System.currentTimeMillis().toString() else "0"
+version = if (isSnapshot) "$baseVersion-$buildNumber" else baseVersion
+group = "org.cthing"
+description = "A Java library for counting lines of source code."
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(libs.versions.java.get())
+    }
+}
+
+dependencies {
+    implementation(libs.commonsIO)
+    implementation(libs.cthingAnnots)
+    implementation(libs.filevisitor)
+    implementation(libs.jacksonDatabind)
+    implementation(libs.jsr305)
+
+    testImplementation(libs.assertJ)
+    testImplementation(libs.equalsVerifier)
+    testImplementation(libs.junitApi)
+    testImplementation(libs.junitParams)
+    testImplementation(libs.mockito)
+
+    testRuntimeOnly(libs.junitEngine)
+    testRuntimeOnly(libs.junitLauncher)
+
+    spotbugsPlugins(libs.spotbugsContrib)
+}
+
+checkstyle {
+    toolVersion = libs.versions.checkstyle.get()
+    isIgnoreFailures = false
+    configFile = file("dev/checkstyle/checkstyle.xml")
+    configDirectory = file("dev/checkstyle")
+    isShowViolations = true
+}
+
+spotbugs {
+    toolVersion = libs.versions.spotbugs
+    ignoreFailures = false
+    effort = Effort.MAX
+    reportLevel = Confidence.MEDIUM
+    excludeFilter = file("dev/spotbugs/suppressions.xml")
+}
+
+jacoco {
+    toolVersion = libs.versions.jacoco.get()
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+val generatedSrcDir = file("generated-src")
+
+tasks {
+    withType<JavaCompile> {
+        options.release = libs.versions.java.get().toInt()
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-options", "-Werror"))
+    }
+
+    named("clean", Delete::class).configure {
+        delete(generatedSrcDir)
+    }
+
+    withType<Jar> {
+        manifest.attributes(mapOf("Implementation-Title" to project.name,
+                                  "Implementation-Vendor" to "C Thing Software",
+                                  "Implementation-Version" to project.version))
+    }
+
+    withType<Javadoc> {
+        val year = SimpleDateFormat("yyyy", Locale.ENGLISH).format(Date())
+        with(options as StandardJavadocDocletOptions) {
+            breakIterator(false)
+            encoding("UTF-8")
+            bottom("Copyright &copy; $year C Thing Software")
+            addStringOption("Xdoclint:all,-missing", "-quiet")
+            addStringOption("Werror", "-quiet")
+            memberLevel = JavadocMemberLevel.PUBLIC
+            outputLevel = JavadocOutputLevel.QUIET
+        }
+    }
+
+    spotbugsMain {
+        reports.create("html").required = true
+    }
+
+    spotbugsTest {
+        isEnabled = false
+    }
+
+    withType<JacocoReport> {
+        dependsOn("test")
+        with(reports) {
+            xml.required = false
+            csv.required = false
+            html.required = true
+            html.outputLocation = layout.buildDirectory.dir("reports/jacoco")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    withType<GenerateModuleMetadata> {
+        enabled = false
+    }
+
+    dependencyUpdates {
+        revision = "release"
+        gradleReleaseChannel = "current"
+        outputFormatter = "plain,xml,html"
+        outputDir = layout.buildDirectory.dir("reports/dependencyUpdates").get().asFile.absolutePath
+
+        rejectVersionIf {
+            isNonStable(candidate.version)
+        }
+    }
+}
+
+val mainSourceSet: SourceSet = extensions.getByType<JavaPluginExtension>().sourceSets[SourceSet.MAIN_SOURCE_SET_NAME]
+val javaDirSet: SourceDirectorySet = mainSourceSet.java
+javaDirSet.setSrcDirs(javaDirSet.srcDirs.plus(generatedSrcDir))
+
+val sourceJar by tasks.registering(Jar::class) {
+    dependsOn(LanguagePlugin.GENERATE_LANGUAGE_TASK)
+    from(project.sourceSets["main"].allSource)
+    archiveClassifier = "sources"
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    from(tasks.getByName("javadoc"))
+    archiveClassifier = "javadoc"
+}
+
+publishing {
+    publications {
+        register("jar", MavenPublication::class) {
+            from(components["java"])
+
+            artifact(sourceJar)
+            artifact(javadocJar)
+
+            pom {
+                name = project.name
+                description = project.description
+                url = "https://github.com/cthing/${project.name}"
+                licenses {
+                    license {
+                        name = "Apache License, Version 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "baron"
+                        name = "Baron Roberts"
+                        email = "baron@cthing.com"
+                        organization = "C Thing Software"
+                        organizationUrl = "https://www.cthing.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/cthing/${project.name}.git"
+                    developerConnection = "scm:git:git@github.com:cthing/${project.name}.git"
+                    url = "https://github.com/cthing/${project.name}"
+                }
+                issueManagement {
+                    system = "GitHub Issues"
+                    url = "https://github.com/cthing/${project.name}/issues"
+                }
+            }
+        }
+    }
+
+    val repoUrl = if (isSnapshot) findProperty("cthing.nexus.snapshotsUrl") else findProperty("cthing.nexus.candidatesUrl")
+    if (repoUrl != null) {
+        repositories {
+            maven {
+                name = "CThingMaven"
+                setUrl(repoUrl)
+                credentials {
+                    username = property("cthing.nexus.user") as String
+                    password = property("cthing.nexus.password") as String
+                }
+            }
+        }
+    }
+}
+
+if (hasProperty("signing.keyId") && hasProperty("signing.password") && hasProperty("signing.secretKeyRingFile")) {
+    signing {
+        sign(publishing.publications["jar"])
+    }
+}
