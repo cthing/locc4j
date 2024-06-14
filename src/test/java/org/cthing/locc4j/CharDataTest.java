1f01c0893d5c7c620542729715d53b95e2a19905
Baron Roberts <baron@cthing.com>
2024-06-13 20:48:22 -0700

    Use SPDX license header.

--- src/test/java/org/cthing/locc4j/CharDataTest.java
+++ src/test/java/org/cthing/locc4j/CharDataTest.java
@@ -1,17 +1,6 @@
 /*
  * Copyright 2024 C Thing Software
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.cthing.locc4j;
@@ -61,6 +50,7 @@ public class CharDataTest {
     }
 
     @Test
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public void testCharAt() {
         final String data = "hello";
         final CharData buffer = new CharData(data.toCharArray());
@@ -102,6 +92,7 @@ public class CharDataTest {
     }
 
     @Test
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public void testSubSequenceStartEnd() {
         final String data = "hello you";
         final CharData buffer = new CharData(data.toCharArray());
--- src/test/java/org/cthing/locc4j/FileCounterTest.java
+++ src/test/java/org/cthing/locc4j/FileCounterTest.java
@@ -1,17 +1,6 @@
 /*
  * Copyright 2024 C Thing Software
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.cthing.locc4j;
@@ -36,7 +25,6 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static org.assertj.core.api.Assertions.entry;
 
 
-@SuppressWarnings("MethodOnlyUsedFromInnerClass")
 public class FileCounterTest {
 
     @Test
@@ -75,7 +63,6 @@ public class FileCounterTest {
 
     @ParameterizedTest
     @ArgumentsSource(FileDataProvider.class)
-    @SuppressWarnings("AssignmentToStaticFieldFromInstanceMethod")
     public void testCount(final ArgumentsAccessor accessor) throws IOException {
         final int numLanguageParams = accessor.size() - 2;
         if (numLanguageParams % 4 != 0) {
